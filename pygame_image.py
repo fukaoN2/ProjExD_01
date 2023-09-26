4a6dfe9f46fe41701a36116375b0509053688790
fukao <c0b2212566@edu.teu.ac.jp>
2023-09-26 15:23:58 +0900

    3まで

---
+++ pygame_image.py
@@ -0,0 +1,30 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    chr_img = pg.image.load("ex01/fig/3.png")
+    chr_img = pg.transform.flip(chr_img, True, False)
+    chr_img = pg.transform.rotozoom(chr_img, 10, 1.0)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        
+
+        screen.blit(bg_img, [0, 0])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
