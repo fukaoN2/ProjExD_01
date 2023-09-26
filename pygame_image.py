b6b722fdcc760421b3ef93697cafa2b1829a7c89
fukao <c0b2212566@edu.teu.ac.jp>
2023-09-26 16:58:43 +0900

    演習課題完成版

--- pygame_image.py
+++ pygame_image.py
@@ -13,8 +13,13 @@ def main():
     #こうかとん初期化
     chr_img = pg.image.load("ex01/fig/3.png")
     chr_img = pg.transform.flip(chr_img, True, False)
-    chr_img2 = pg.transform.rotozoom(chr_img, 10, 1.0)
-    chr_imgs = [chr_img, chr_img2]
+    #chr_img2 = pg.transform.rotozoom(chr_img, 10, 1.0)
+    chr_imgs = []
+    for i in range(0, 11, 1):
+        chr_img2 = pg.transform.rotozoom(chr_img, i, 1.0)
+        chr_imgs.append(chr_img2)
+
+    count = 0
 
     while True:
         for event in pg.event.get():
@@ -25,10 +30,14 @@ def main():
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [1600-x, 0])
         screen.blit(bg_img, [3200-x, 0])
-        screen.blit(chr_imgs[tmr%2], [300, 200])
+        screen.blit(chr_imgs[count], [300, 200])
         pg.display.update()
         tmr += 1
-        clock.tick(1000)
+        count += 1
+        if count == len(chr_imgs):
+            count = 0
+
+        clock.tick(200)
 
 
 if __name__ == "__main__":
