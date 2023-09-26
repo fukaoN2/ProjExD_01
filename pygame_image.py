365a2eae1818c40a706a49b86a191d480a9d857f
fukao <c0b2212566@edu.teu.ac.jp>
2023-09-26 16:16:26 +0900

    練習問題完成+ 雲の流れ滑らか

--- pygame_image.py
+++ pygame_image.py
@@ -7,6 +7,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     tmr = 0
 
     #こうかとん初期化
@@ -20,12 +21,14 @@ def main():
             if event.type == pg.QUIT:
                 return
 
-        x = -tmr
-        screen.blit(bg_img, [x, 0])
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [1600-x, 0])
+        screen.blit(bg_img, [3200-x, 0])
         screen.blit(chr_imgs[tmr%2], [300, 200])
         pg.display.update()
         tmr += 1
-        clock.tick(100)
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
